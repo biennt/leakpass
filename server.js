7d865fc38b8d2e461302964cbfeb1de181730f85
Bien <bien@bienlab.com>
2021-08-21 23:10:59 +0700

    fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-//var sha1 = require('js-sha1');
 var sha1 = require('./models/sha1');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,21 +18,27 @@ app.use('/api', router);
 
 router.get('/:hashedpass', function(req, res){
 var txthashedpass = req.params.hashedpass.toUpperCase();
-	sha1.findOne({ "hashpass": txthashedpass }, function(err, sha1s ){
-		if (err) {
-			console.log('Unexpected error. URI=' + req.url);
-			res.status(500).send('Unexpected error');
-		} else {
-			if (sha1s == null) {
-				console.log('Not found! URI=' + req.url);
-				res.status(404).send('Not found');
-			} else {
-			    console.log('Found! URI=' + req.url);
-			    res.json(sha1s);
-		    }
-		}
-	});
+if (txthashedpass.length === 40) {
+  sha1.findOne({ "hashpass": txthashedpass }, function(err, sha1s ){
+    if (err) {
+      console.log('Unexpected error. URI=' + req.url);
+      res.status(500).send('Unexpected error');
+    } else {
+      if (sha1s == null) {
+        console.log('Not found! URI=' + req.url);
+        res.status(404).json({ count: 0});
+      } else {
+        console.log('Found! URI=' + req.url);
+        res.json(sha1s);
+      }
+    }
+    });
+} else {
+  console.log('not a valid sha1 hash. URI=' + req.url);
+  res.status(500).send(txthashedpass + ' is not a valid sha1 hash');
+}
 });
 
 app.listen(port);
 console.log('API server listening on port ' + port);
+
